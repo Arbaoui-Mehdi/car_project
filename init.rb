8784f8273ee7aa14a8d987697d6a3573a694dd36
Arbaoui Mehdi <arbaoui.mehdi@gmail.com>
2017-06-13 18:45:10 -0700

    First Steps and Concepts

---
+++ init.rb
@@ -0,0 +1,23 @@
+#!usr/bin/env ruby
+
+require_relative('lib/car')
+
+puts
+puts "Enter information for the car you want to pushase."
+puts
+
+print "Make: "
+make = gets.chomp
+
+print "Year: "
+year = gets.chomp
+
+print "Color: "
+color = gets.chomp
+
+car = Car.new(:make => make, :year => year, :color => color)
+
+puts
+puts "I Understand that you want to purchase: #{car.full_name}"
+puts
+
---
+++ spec/car_spec.rb
@@ -0,0 +1,52 @@
+require 'car'
+
+describe Car do
+	
+	let(:car) { Car.new }
+	
+	describe 'attributes' do
+
+		it "allows reading and writing for :make" do
+			car.make = 'Test'
+			expect(car.make).to eq('Test')
+		end
+
+		it "allows reading and writing for :year" do
+			pending('Debugging a problem')
+			car.year = 2017
+			expect(car.year).to eq(2017)	
+		end
+
+		it "allows reading and writing for :color" do
+			car.color = 'foo'
+			expect(car.color).to eq('foo')
+		end
+
+		it "allows writing for :doors"
+
+	end
+
+	describe '.colors' do
+		
+		it "returns an array of color names" do
+			c = ['blue', 'black', 'red', 'green']
+			expect(Car.colors).to match_array(c)
+		end
+
+	end
+
+	describe '#full_name' do
+		
+		context "when initialized with no arguments" do
+			it 'returns a string using default values' do
+				expect(car.full_name).to eq('2007 Volvo (unknown)')
+			end
+		end
+
+		it "when initialized with arguments" do
+			@honda = Car.new(:make => 'Honda', :year => 2004, :color => 'blue')
+			expect(@honda.full_name).to eq("2004 Honda (blue)")
+	 end	 
+	end
+
+end
