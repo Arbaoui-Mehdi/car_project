c62474c546407705a678b407f0e008e46ef6ecb8
Mehdi <whithat87@gmail.com>
2017-06-14 04:56:35 +0000

    Numeric Comparison Matchers

--- spec/expectation_matchers_spec.rb
+++ spec/expectation_matchers_spec.rb
@@ -1,6 +1,8 @@
 describe 'Expectation machers' do
 
-
+  #
+  #
+  # Equivalence Matchers
   describe 'equivalent matchers' do
 
 		it 'will match loose equality with #eq' do
@@ -35,5 +37,59 @@ describe 'Expectation machers' do
 		end
 
   end
-  
+
+  #
+  #
+  # Truthiness Matchers
+  describe 'thrutiness matchers' do
+
+    it 'will match true/false' do
+			expect(1 < 2).to be(true)        # do not use 'be_true'
+      expect(1 > 2).to be(false)       # do not use 'be_false'
+
+      expect('foo').not_to be(true)    # the string is not exactly true
+      expect(nil).not_to be(false)     # nil is not exactly
+      expect(0).not_to be(false)       # 0 is not exactly false
+    end
+
+    it 'will match truthy/falsy' do
+			expect(1 < 2).to be_truthy
+      expect(1 > 1).to be_falsey
+
+      expect('foo').to be_truthy
+      expect(nil).to be_falsey
+      expect(0).to_not be_falsey
+    end
+
+    it 'will match nil' do
+      expect(nil).to be_nil
+      expect(nil).to be(nil)
+
+      expect(false).not_to be_nil
+      expect(0).not_to be_nil
+    end
+
+  end
+
+  #
+  #
+  # Numeric Comparison Machers
+  describe 'numeric comparison matchers' do
+
+    it 'will match less than/greater than' do
+			expect(10).to be > 9
+			expect(10).to be >=10
+			expect(10).to be <= 10
+			expect(9).to be < 10
+    end
+
+    it 'will match numeric ranges' do
+      expect(5).to be_between(5, 10).inclusive     # include 5 and 10 from the range
+      expect(5).not_to be_between(5, 10).exclusive # exclude 5 and 10 from the range
+    	expect(10).to be_within(1).of(11)
+      expect(5..10).to cover(9)
+    end
+
+  end
+
 end
