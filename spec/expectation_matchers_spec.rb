49a3dcf836b60e221a57dfc7952a283bc91f637b
Arbaoui Mehdi <arbaoui.mehdi@gmail.com>
2017-06-13 19:35:13 -0700

    Expectation Matchers

---
+++ spec/expectation_matchers_spec.rb
@@ -0,0 +1,34 @@
+describe 'Expectation machers' do
+	
+	it 'will match loose equality with #eq' do
+		a = '2 cats'
+		b = '2 cats'
+		expect(a).to eq(b)
+		expect(a).to be == b     # synonym for #eq
+		
+		c = 17
+		d = 17.0
+		expect(c).to eq(d)       # different types, but 'close' enough
+	end
+
+	it 'will match equality with #eql' do
+		a = '2 cats'
+		b = '2 cats'
+		expect(a).to eql(b)      # just a little stricter
+
+		c = 17
+		d = 17.0
+		expect(c).not_to eql(d)  # not the same, close doesn't count
+	end
+
+	it 'will match identity equality with #equal' do
+		a = '2 cats'
+		b = '2 cats'
+		expect(a).not_to equal(b) # same value, but different object
+
+		c = b
+		expect(b).to equal(c)     # same object
+		expect(b).to be(c)        # synonym for equal
+	end
+
+end
