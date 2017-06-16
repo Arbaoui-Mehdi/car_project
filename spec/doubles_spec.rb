7bf632820c1f511246f7b35a392316c865eb37c9
Arbaoui Mehdi <arbaoui.mehdi@gmail.com>
2017-06-15 19:32:28 -0700

    Doubles with message count constraints

--- spec/doubles_spec.rb
+++ spec/doubles_spec.rb
@@ -161,7 +161,7 @@ describe 'Doubles' do
   #
   #
   # Argument Constraints
-  context 'with argument constrains' do
+  context 'with argument constraints' do
 
     it 'expects arguments will match' do
       dbl = double('Customer List')
@@ -196,6 +196,58 @@ describe 'Doubles' do
         boolean
       )
       dbl.sort('name', 'asc', true)
+    end
+
+  end
+
+  #
+  #
+  # Message Count Constraints
+  context 'with message count constraints' do
+
+    it 'allows constraints on message count' do
+
+      class Cart
+
+        def initialize
+          @items = []
+        end
+
+        def add_item(id)
+          @items << id
+        end
+
+        def restock_item(id)
+          @items.delete(id)
+        end
+
+        def empty
+          @items.each {
+            | id | restock_item(id)
+          }
+        end
+
+
+
+      end
+
+      cart = Cart.new
+      cart.add_item(35)
+      cart.add_item(178)
+
+      expect(cart).to receive(:restock_item).twice
+      cart.empty
+
+    end
+
+    it 'allows using at_least/at_most' do
+      post = double('BlogPost')
+      expect(post).to receive(:like).at_least(3).times
+
+      post.like(:user => 'Bob')
+      post.like(:user => 'Mary')
+      post.like(:user => 'Ted')
+      post.like(:user => 'Jane')
     end
 
   end
